8a05ed1d74655e1e5d66806a9077d769511e4b63
Leonardo Pérez <ephelsa@hotmail.com>
2017-09-18 07:42:01 -0500

    fixed calls to api 23

--- app/src/main/java/co/edu/udea/estructuras/landresperez/citophoneapp/Activity/UserList.java
+++ app/src/main/java/co/edu/udea/estructuras/landresperez/citophoneapp/Activity/UserList.java
@@ -1,23 +1,21 @@
 package co.edu.udea.estructuras.landresperez.citophoneapp.Activity;
 
 import android.Manifest;
+import android.annotation.TargetApi;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,6 +24,8 @@ import co.edu.udea.estructuras.landresperez.citophoneapp.Data.UserListData;
 import co.edu.udea.estructuras.landresperez.citophoneapp.R;
 
 public class UserList extends AppCompatActivity {
+
+    private final int REQUEST_PERMISSION_CALL_PHONE = 1;
 
     private Button callButton;
 
@@ -58,18 +58,24 @@ public class UserList extends AppCompatActivity {
         });
     }
 
+    @TargetApi(23)
     private void callMethod() {
         try {
+            int permissionCheck = ContextCompat.
+                    checkSelfPermission(this, Manifest.permission.CALL_PHONE);
+
             Intent callIntent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + 31504093));
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
+            if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.CALL_PHONE)) {
+                    return;
+                } else {
+                    requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, REQUEST_PERMISSION_CALL_PHONE);
+                }
+            } else {
+                Toast.makeText(this, "Permission (already) Granted!", Toast.LENGTH_SHORT).show();
             }
+
             startActivity(callIntent);
         } catch (ActivityNotFoundException e) {
             Log.e("Llamada", "Call failed", e);
