39a40c996d09fbed5a1709c8342be81c7117c4b7
Leonardo Pérez <ephelsa@hotmail.com>
2017-09-19 18:23:26 -0500

    Login functional

--- citophoneapp/src/main/java/co/edu/udea/estructuras/landresperez/citophoneapp/Activity/Block.java
+++ citophoneapp/src/main/java/co/edu/udea/estructuras/landresperez/citophoneapp/Activity/Block.java
@@ -11,6 +11,11 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,6 +34,12 @@ public class Block extends AppCompatActivity {
     private LinearLayoutManager linearLayoutManager;
     private List<BlockData> blockDataList;
 
+    // Firebase methods
+    // Write a message to the database
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,6 +52,38 @@ public class Block extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
 
         iniciar();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        blockDataList = new ArrayList<>();
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("Block", "Value is: " + value);
+
+
+                blockDataList.add(new BlockData("Bloque " + String.valueOf(+ 1)));
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("Block", "Failed to read value.", error.toException());
+            }
+        });
+
+        BlockAdapter blockAdapter = new BlockAdapter(blockDataList);
+
+        recyclerView.setAdapter(blockAdapter);
     }
 
     @Override
@@ -64,16 +107,7 @@ public class Block extends AppCompatActivity {
 
     private void iniciar() {
         // Delete this method when join with Firebase
-        blockDataList = new ArrayList<>();
 
-        for (int i = 0; i < 4; i++) {
-            blockDataList.add(new BlockData("Bloque " + String.valueOf(i + 1)));
-            Log.w("block_1", "Iniciar");
-        }
-
-        BlockAdapter blockAdapter = new BlockAdapter(blockDataList);
-
-        recyclerView.setAdapter(blockAdapter);
     }
 
     /*
