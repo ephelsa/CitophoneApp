310a7e6e5618c0742b86b55277e00e15fb8308a4
Leonardo Pérez <ephelsa@hotmail.com>
2017-09-21 07:47:14 -0500

    Functional app.
    Now, it's time to test the call forwarding.
    
    Pray for this, please.

--- citophoneapp/src/main/java/co/edu/udea/estructuras/landresperez/citophoneapp/Activity/LoginView.java
+++ citophoneapp/src/main/java/co/edu/udea/estructuras/landresperez/citophoneapp/Activity/LoginView.java
@@ -71,11 +71,11 @@ public class LoginView extends FragmentActivity implements View.OnClickListener 
 
             if (pass.length() >= 6) {
                 login(user, pass);
-            } else if (pass.length() == 0 && user.length() != 0) {
-                Toast.makeText(getApplicationContext(), R.string.void_fields,
-                        Toast.LENGTH_SHORT).show();
-            } else {
+            } else if(pass.length() < 6 && !user.equals(EMAIL_HANDLER)) {
                 Toast.makeText(getApplicationContext(), R.string.short_password,
+                        Toast.LENGTH_SHORT).show();
+            } else if (pass.length() == 0 || pass.length() < 6) {
+                Toast.makeText(getApplicationContext(), R.string.void_fields,
                         Toast.LENGTH_SHORT).show();
             }
         }
