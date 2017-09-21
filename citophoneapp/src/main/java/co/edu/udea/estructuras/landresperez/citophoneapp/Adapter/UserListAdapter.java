58221d5b711e9c64516f95bd821bae42f3be1cc9
Leonardo Pérez <ephelsa@hotmail.com>
2017-09-21 10:05:18 -0500

    server_citophoneapp
    Read from database

--- citophoneapp/src/main/java/co/edu/udea/estructuras/landresperez/citophoneapp/Adapter/UserListAdapter.java
+++ citophoneapp/src/main/java/co/edu/udea/estructuras/landresperez/citophoneapp/Adapter/UserListAdapter.java
@@ -133,10 +133,9 @@ public class UserListAdapter extends RecyclerView.Adapter<UserListAdapter.ViewHo
                 }
             } else {
                 myRef.setValue(number);
+
                 Thread.sleep(2000);
-
                 myRef.setValue("#");
-
 
                 Toast.makeText(context, R.string.calling, Toast.LENGTH_SHORT).show();
             }
